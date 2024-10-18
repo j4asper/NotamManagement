e9044a0dd89d6936e509a17549569c48f25cc996
Rasmus Kaer <kaer@joergensenfam.dk>
2024-10-18 10:00:28 +0200

    change shared to core

---
+++ NotamManagement.Core/Data/NotamManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotamManagement.Core.Models;
+
+namespace NotamManagement.Core.Data
+{
+    public class NotamManagementContext : DbContext
+    {
+        public DbSet<Shared.Models.Notam> Notams { get; set; }
+
+
+
+
+    }
+}
