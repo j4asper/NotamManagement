6492ce49484b3a599566c792597c5660eb4a49d2
Rasmus Kaer <kaer@joergensenfam.dk>
2024-10-18 10:18:55 +0200

    Setup EF Core

--- NotamManagement.Core/Data/NotamManagementContext.cs
+++ NotamManagement.Core/Data/NotamManagementContext.cs
@@ -10,7 +10,19 @@ namespace NotamManagement.Core.Data
 {
     public class NotamManagementContext : DbContext
     {
-        public DbSet<Shared.Models.Notam> Notams { get; set; }
+        public DbSet<Notam> Notams { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Coordinates> Coordinates { get; set; }
+        public DbSet<FlightPlan> FlightPlans { get; set; }
+        public DbSet<NotamAction> NotamActions { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql($"Host=Hjem.jazper.dk;Database=notam_management;Username=master;Password=9ycLQnsHBy34gtI");
+        }
 
 
 
