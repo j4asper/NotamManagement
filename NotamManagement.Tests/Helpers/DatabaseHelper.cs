bc13d96d796c18507972ec5ebe31645799a21128
Jasper Christiansen <jasper@jazper.dk>
2024-10-23 11:43:26 +0200

    Added tests for NotamRepository (#47)
    * Added tests for NotamRepository
    
    * Add GetByIdTest

---
+++ NotamManagement.Tests/Helpers/DatabaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotamManagement.Core.Data;
+
+namespace NotamManagement.Tests.Helpers;
+
+public static class DatabaseHelper
+{
+    public static NotamManagementContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<NotamManagementContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new NotamManagementContext(options);
+    }
+}
