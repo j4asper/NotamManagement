8ab5399f02547d70d2af07a2bafbca9a961a28d7
Jasper Christiansen <jasper@jazper.dk>
2024-10-28 13:39:26 +0100

    Added location + IsPermanent property

--- NotamManagement.Core/Models/Notam.cs
+++ NotamManagement.Core/Models/Notam.cs
@@ -1,8 +1,12 @@
-﻿namespace NotamManagement.Core.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace NotamManagement.Core.Models;
 
 public class Notam
 {
     public int Id { get; set; }
+    
+    public required string Location { get; set; }
     
     public DateTime ValidFrom { get; set; }
     
@@ -29,11 +33,15 @@ public class Notam
     public required string Scope { get; set; }
 
     public string? NotamText { get; set; }
-
+    
+    [JsonIgnore]
+    public bool IsPermanent => ValidTo > new DateTime(9995,12,31);
+    
+    [JsonIgnore]
     public string? RawNotam => $"{Identifier} NOTAM{Type.ToString().Substring(0, 1)} {ReferenceIdentifier ?? String.Empty} \n" +
         $"Q) {FIR}/Q{QCode}/{Traffic}/{Purpose}/{Scope}/{Coordinates.LowerFlightLevel.ToString("D3")}/{Coordinates.UpperFlightLevel.ToString("D3")}/{Coordinates.LongLat}/{Coordinates.Radius.ToString("D3")} \n" +
         $"A) {NotamOffice}\n" +
-        $"B) {ValidFrom.ToString("yyMMddHHmm")} \n{(ValidTo>new DateTime(9995,12,31) ? "C) PERM" : ValidTo.HasValue ? $"C) {ValidTo?.ToString("yyMMddHHmm")}" : "")} \n" +
+        $"B) {ValidFrom.ToString("yyMMddHHmm")} \n{(IsPermanent ? "C) PERM" : ValidTo.HasValue ? $"C) {ValidTo?.ToString("yyMMddHHmm")}" : "")} \n" +
         $"E) {NotamText}";
 
     public required Coordinates Coordinates { get; set; }
