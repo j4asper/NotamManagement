dabab8c8b7737416187b939dd315af46507ade29
Rasmus Kaer <kaer@joergensenfam.dk>
2024-11-05 11:22:20 +0100

    add find notamaction from location

--- NotamManagement.Api/Controllers/NotamActionController.cs
+++ NotamManagement.Api/Controllers/NotamActionController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NotamManagement.Core.Models;
 using NotamManagement.Core.Repository;
+using System.Linq.Expressions;
 using System.Security.Claims;
 using System.Web;
 
@@ -12,12 +14,14 @@ namespace NotamManagement.Api.Controllers;
 public class NotamActionController : ControllerBase
 {
     private readonly IRepository<NotamAction> _notamActionRepository;
+    private readonly IRepository<Notam> _notamRepository;
     private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public NotamActionController(IRepository<NotamAction> notamActionRepository, IHttpContextAccessor httpContextAccessor)
+    public NotamActionController(IRepository<NotamAction> notamActionRepository, IHttpContextAccessor httpContextAccessor, IRepository<Notam> notamRepository)
     {
         _notamActionRepository = notamActionRepository;
         _httpContextAccessor = httpContextAccessor;
+        _notamRepository = notamRepository;
     }
 
 
@@ -81,4 +85,26 @@ public class NotamActionController : ControllerBase
         await _notamActionRepository.AddAsync(notamAction);
         return Ok(notamAction);
     }
+
+    [HttpGet("find")]
+    public async Task<ActionResult<IReadOnlyList<NotamAction>>> FindByLocationAsync([FromQuery] string location)
+    {
+        if (string.IsNullOrEmpty(location))
+        {
+            return BadRequest("Location parameter is required.");
+        }
+
+        // Fetch all Notams for the given location
+        var result = await _notamRepository.FindAsync(x => x.Location == location);
+
+        // Collect all NotamIds from the result
+        var notamIds = result.Select(item => item.Id).ToList();
+
+        // Fetch all NotamActions in a single call using the collected NotamIds
+        var notamActions = await _notamActionRepository.FindAsync(x => notamIds.Contains(x.NotamId));
+
+        return Ok(notamActions);
+    }
+
+
 }
